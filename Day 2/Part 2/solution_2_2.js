282272127d3d63c4ae1a72c9d9b2767ef6c7edf8
Adam Pearson <adam.pearson94@gmail.com>
2024-12-03 00:06:47 +0000

    Added solution for day 2 part 2

---
+++ Day 2/Part 2/solution_2_2.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+
+function readReportsFromFile() {
+    return fs.readFileSync('../resources/input.txt', 'utf-8');
+}
+
+function parseReports(reports) {
+    return reports.split('\n').map(levels => levels.split(' ').map(n => parseInt(n)));
+}
+
+function checkReport(report, allowFurtherRecursion = true) {
+    let direction;
+
+    const result = report.reduce((isSafe, curr, i) => {
+        if (i === 0) return true;
+        if (isSafe === false) return false;
+
+        if (curr === report[i - 1]) {
+            return false;
+        };
+
+        if (!direction) {
+            direction = curr > report[i - 1] ? 1 : -1;
+        } else if ((curr > report[i - 1] ? 1 : -1) !== direction) {
+            return false;
+        }
+
+        if (Math.abs(curr - report[i - 1])  > 3) {
+            return false
+        };
+
+        return true;
+    }, true)
+
+    if (!result && allowFurtherRecursion) {
+        for (let i = 0; i < report.length; i++) {
+            const newReport = [...report];
+            newReport.splice(i, 1);
+            if (checkReport(newReport, false)) {
+                return true;
+            }
+        }
+    };
+    return result;
+}
+
+function checkSafenessOfReports(reports) {
+    return parseReports(reports).map(report => checkReport(report));
+}
+
+function countSafeReports(reportResults) {
+    return reportResults.filter(result => result).length;
+}
+
+const reports = readReportsFromFile();
+console.log(countSafeReports(checkSafenessOfReports(reports)));
