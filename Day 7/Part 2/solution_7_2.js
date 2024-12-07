6b087abb13927041ab2fa5ad7eb3dc6d6fdfe2c2
Adam Pearson <adam.pearson94@gmail.com>
2024-12-07 17:40:59 +0000

    7.2 done

---
+++ Day 7/Part 2/solution_7_2.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const parsedInput = fs.readFileSync('../resources/input.txt', 'utf-8').split("\n").map(l => l.split(": "));
+
+const start = performance.now();
+
+const backtrack = (goal, numbers, curr = 0, index = 0) => {
+    if (index >= numbers.length) return curr === goal;
+
+    const next = numbers[index];
+
+    const nextSum = curr + next;
+    const nextProduct = curr * next;
+    const nextConcat = curr > 0 ? parseInt(String(curr) + String(next)) : next;
+
+    const sumRes = backtrack(goal, numbers, nextSum, index + 1);
+    const prodRes = nextProduct > 0 ? backtrack(goal, numbers, nextProduct, index + 1) : false;
+    const concatRes = backtrack(goal, numbers, parseInt(nextConcat), index + 1);
+
+    return (sumRes || prodRes || concatRes);
+};
+
+const result = parsedInput.reduce((acc, curr) => {
+    const goal = +curr[0];
+    const numbers = curr[1].split(" ").map(num => +num);
+
+    if (numbers.length === 1) {
+        return acc + (numbers[0] === goal ? goal : 0);
+    }
+
+    if (backtrack(goal, numbers)) {
+        return acc + goal;
+    };
+
+    return acc;
+}, 0);
+
+console.log('result: ', result.toString());
+
+const end = performance.now();
+console.log(`Execution time: ${end - start} ms`);
