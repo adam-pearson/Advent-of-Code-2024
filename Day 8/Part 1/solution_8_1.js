fc42b578fcbbacfcaeec2cb69d3bed01e181de7f
Adam Pearson <adam.pearson94@gmail.com>
2024-12-08 23:16:29 +0000

    tweaked part 1 solution slightly

--- Day 8/Part 1/solution_8_1.js
+++ Day 8/Part 1/solution_8_1.js
@@ -8,39 +8,41 @@ const antennaMap = {};
 const antinodes = new Set();
 
 parsedInput.forEach((row, rowIndex) => row.forEach((cell, colIndex) => {
-    if (cell !== '.') {
-        if (!antennaMap[cell]) antennaMap[cell] = [];
-        antennaMap[cell].push({x: colIndex, y: rowIndex});
-    }
+  if (cell !== '.') {
+      if (!antennaMap[cell]) antennaMap[cell] = [];
+      antennaMap[cell].push({x: colIndex, y: rowIndex});
+  }
 }));
 
-function antinodesInBounds({ x, y }, gridWidth, gridHeight) {
-    return x >= 0 && x < gridWidth && y >= 0 && y < gridHeight;
-  }
+
+function inBounds({ x, y }) {
+  return x >= 0 && x < gridWidth && y >= 0 && y < gridHeight;
+}
 
 for (const frequency in antennaMap) {
-    const positions = antennaMap[frequency];
+  console.log(frequency, antennaMap[frequency])
+  const positions = antennaMap[frequency];
 
-    positions.forEach(pos1 => positions.forEach(pos2 => {
-        const { x: x1, y: y1 } = pos1;
-        const { x: x2, y: y2 } = pos2;
+  positions.forEach(pos1 => positions.forEach(pos2 => {
+      const { x: x1, y: y1 } = pos1;
+      const { x: x2, y: y2 } = pos2;
 
-        const dx = x2 - x1;
-        const dy = y2 - y1;
+      const dx = x2 - x1;
+      const dy = y2 - y1;
 
-        if (dx !== 0 || dy !== 0) {
-          const antinode1 = { x: x1 - dx, y: y1 - dy };
-          const antinode2 = { x: x2 + dx, y: y2 + dy };
+      if (dx !== 0 && dy !== 0) {
+        const possibleAntinodes = [];
+          possibleAntinodes.push({ x: x1 - dx, y: y1 - dy });
+          possibleAntinodes.push({x: x2 + dx, y: y2 + dy});
 
-          if (antinodesInBounds(antinode1, gridWidth, gridHeight)) {
-            antinodes.add(`${antinode1.x},${antinode1.y}`);
-          }
-          
-          if (antinodesInBounds(antinode2, gridWidth, gridHeight)) {
-            antinodes.add(`${antinode2.x},${antinode2.y}`);
-          }
-        }
-    }))        
+          possibleAntinodes.forEach(antinode => {
+            if (!antinodes.has(`${antinode.x},${antinode.y}`) && inBounds(antinode)) {
+              antinodes.add(`${antinode.x},${antinode.y}`);
+            }
+          });
+      }
+
+  }));
 }
 
 console.log(antinodes.size);
